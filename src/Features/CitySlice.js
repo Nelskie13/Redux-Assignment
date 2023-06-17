45606e126f125666e29f515273be9b8b4cfcabff
Jonel Capa <jonelcapa@yahoo.com>
2023-06-17 19:58:13 +0800

    changes

--- src/Features/CitySlice.js
+++ src/Features/CitySlice.js
@@ -1,23 +1,43 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+const apiKey = "a05c7c2a2aae40feb82131443230105";
+
+export const fetchSuggestedCities = createAsyncThunk(
+  "city/fetchSuggestedCities",
+  async (location) => {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/search.json?key=${apiKey}&q=${location}`
+    );
+    const data = await response.json();
+    return data;
+  }
+);
 
 const initialState = {
-  name: "",
+  cityName: "",
   suggestedCities: [],
+  isLoading: false,
+  error: null,
 };
 
 const citySlice = createSlice({
   name: "city",
   initialState,
-  reducers: {
-    setName: (state, action) => {
-      state.name = action.payload;
-    },
-    setSuggestedCities: (state, action) => {
-      state.suggestedCities = action.payload;
-    },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchSuggestedCities.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(fetchSuggestedCities.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.suggestedCities = action.payload;
+      })
+      .addCase(fetchSuggestedCities.rejected, (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   },
 });
-
-export const { setName, setSuggestedCities } = citySlice.actions;
 
 export default citySlice.reducer;
